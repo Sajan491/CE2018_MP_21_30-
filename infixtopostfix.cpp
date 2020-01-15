ab02c8cd9751c3682f583b7aff9e742422cc1e74
sajan491 <sajanmahat491@gmail.com>
2020-01-15 21:34:56 +0545

    all

--- infixtopostfix.cpp
+++ infixtopostfix.cpp
@@ -22,43 +22,43 @@ int priority(char c){
 
 //main block
 int main(){
-  string infixexp,postfixexp;
-	cout<<"Enter an infix expression:"<<endl;
+    string infixexp,postfixexp;
+    cout<<"Enter an infix expression:"<<endl;
 	cin>>infixexp;
-  Stack s;
-  char c;
+    Stack s;
+    char c;
 	for(int i=0;i<infixexp.length();i++){
 		c=infixexp[i];
-    if(c=='('){       // If the scanned character is an ‘(‘, push it to the stack. 
-
-      s.push(c); 
-    }
-    else if(c==')') {       // If the scanned character is an ‘)’, pop to output char p from the stack until an '(' is encountered
-
-      char p=s.pop();
-      while(p!='('){
-        postfixexp+=p;
-        p=s.pop();
-      }
-    }
-    else if(int(c)>41&&int(c)<48){  //If the character is an operator
-      if(!s.isEmpty()){
-        char top=s.peak();
-        while(!s.isEmpty()&&(priority(c)<=priority(top))){ //priority check with top element
-          char token=s.pop();   //If priority of top element is higher or equal
-          postfixexp+=token;
-          top=s.peak();   //Resetting the top value
+        if(c=='('){       // If the scanned character is an ‘(‘, push it to the stack. 
+    
+          s.push(c); 
         }
-      }
-      s.push(c);
-    }
-    else{   //If the character is an operand
-      postfixexp+=c;  
-    }
+        else if(c==')') {       // If the scanned character is an ‘)’, pop to output char p from the stack until an '(' is encountered
+    
+          char p=s.pop();
+          while(p!='('){
+            postfixexp+=p;
+            p=s.pop();
+          }
+        }
+        else if(int(c)>41&&int(c)<48){  //If the character is an operator
+          if(!s.isEmpty()){
+            char top=s.peak();
+            while(!s.isEmpty()&&(priority(c)<=priority(top))){ //priority check with top element
+              char token=s.pop();   //If priority of top element is higher or equal
+              postfixexp+=token;
+              top=s.peak();   //Resetting the top value
+            }
+          }
+          s.push(c);
+        }
+        else{   //If the character is an operand
+          postfixexp+=c;  
+        }
 	}
-  while(!s.isEmpty()){    //popping all the remaining characters at the end
-    char token=s.pop();
-    postfixexp+=token;
-  }
-  cout<<"The equivalent postfix expression is:"<<endl<<postfixexp<<endl;
+    while(!s.isEmpty()){    //popping all the remaining characters at the end
+        char token=s.pop();
+        postfixexp+=token;
+    }
+    cout<<"The equivalent postfix expression is:"<<endl<<postfixexp<<endl;
 }
